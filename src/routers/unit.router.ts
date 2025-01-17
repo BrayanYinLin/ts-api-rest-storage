31b112024df418566bcda34cc6bea76fb0f96062
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-01-17 15:43:54 -0500

    fix: unit router

--- src/routers/unit.router.ts
+++ src/routers/unit.router.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
+import UnitController from '../controllers/unit.controller'
 
 const RouterUnit = Router()
+const ControllerUnit = new UnitController()
+
+RouterUnit.get('/all', ControllerUnit.findAll)
 
 export default RouterUnit
